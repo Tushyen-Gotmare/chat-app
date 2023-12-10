ec0255b79f1dc64214064ed157e00724c588a1bd
Tushyen <tanmaygotmare@gmail.com>
2023-12-10 10:53:15 +0530

    intialized project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+
+import firebase from "firebase/app";
+const config = {
+    apiKey: "AIzaSyD3qpHtpsZANFUX6-YgU-8kfj4-uRpM0FA",
+    authDomain: "chat-web-app-5f26f.firebaseapp.com",
+    projectId: "chat-web-app-5f26f",
+    storageBucket: "chat-web-app-5f26f.appspot.com",
+    messagingSenderId: "254381524311",
+    appId: "1:254381524311:web:f85a2f3395a892d809aa12"
+  };
+
+  const app = firebase.initializeApp(config);
