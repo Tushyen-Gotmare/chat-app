9a65686c3860a84141ebf5622f62f6e55138b180
Tushyen <tanmaygotmare@gmail.com>
2023-12-14 12:11:37 +0530

    sidebar added and dashboard

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DashboardToggle from './dashborad/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+     <div>
+      <DashboardToggle/>
+     </div>
+    </div>
+  )
+}
+
+export default Sidebar;
