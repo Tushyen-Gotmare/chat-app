8faf16d1a0a35d822bbe3e4c6a89e417fa8df8a0
Tushyen <tanmaygotmare@gmail.com>
2023-12-23 10:46:18 +0530

    created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import ChatTop from "../../components/chat-windows/top"
+import Messages from "../../components/chat-windows/messages"
+import ChatBottom from "../../components/chat-windows/bottom"
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { useRooms } from '../../context/Room.context'
+import { Loader } from 'rsuite'
 
 const Chat = () => {
+
+    const {chatId} = useParams();
+    const rooms = useRooms();
+
+    if(!rooms){
+        return <Loader center vertical size="md" content="Loading" speed='slow'/>
+    }
+    const currentRoom = rooms.find(room => room.id === chatId);
+
+    if(!currentRoom){
+        return <h6 className="text-center mt-page">Chat {chatId} not found</h6>
+    }
   return (
-    <div>Chat</div>
+    <>
+        <div className="chat-top">
+            <ChatTop/>
+        </div>
+        <div className="chat-middle">
+            <Messages/>
+        </div>
+        <div className="chat-bottom">
+            <ChatBottom/>
+        </div>
+    </>
   )
 }
 
